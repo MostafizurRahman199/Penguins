946a2d677ac79f7ade49df675d1316750271cdb6
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-03-27 20:42:54 +0600

    navbar

---
+++ src/app/components/Nabvar.js
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function () {
+  return (
+    <nav className="bg-gray-800">
+      <div className="flex h-16 items-center justify-between">
+        <div className="flex flex-shrink-0 items-center ">
+          <Image
+            src="/nav_logo4.png"
+            width={80}
+            height={80}
+            className="block "
+          ></Image>
+          <h1 className="text-white flex items-center px-2 text-2xl font-medium">
+            <Link href={"/"}>Penguins</Link>
+          </h1>
+        </div>
+        <div className="flex space-x-4">
+          <Link
+            href={"/"}
+            className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Home
+          </Link>
+          <Link
+            href={"/"}
+            className="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Testimonials
+          </Link>
+          <Link
+            href={"/"}
+            className="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Price
+          </Link>
+          <Link
+            href={"/"}
+            className="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Projects
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
