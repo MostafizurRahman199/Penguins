c05001573cd2f76a1284af16cd11947ea9e29bde
MostafizurRahman199 <67595254+MostafizurRahman199@users.noreply.github.com>
2024-05-27 20:37:48 +0600

    Add files via upload
    Drak mode added

--- src/app/components/Nabvar.js
+++ src/app/components/Nabvar.js
@@ -50,13 +50,15 @@
 //     </nav>
 //   );
 // }
-"use client"
+"use client";
+
+import React, { useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
 import styles from '@/app/components/main.module.css';
+import DarkMode from "./DarkMode";
 
-export default function Navbar() {
+export default function Navbar({ theme, toggleTheme }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -86,6 +88,12 @@ export default function Navbar() {
               Home
             </Link>
             <Link
+              href={"/SocialMedia"}
+              className="bg-pink-600 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-cyan-400"
+            >
+              Profile
+            </Link>
+            <Link
               href={"/"}
               className="bg-pink-500 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-cyan-400 transition duration-150 ease-in-out"
             >
@@ -103,6 +111,13 @@ export default function Navbar() {
             >
               Projects
             </Link>
+
+            <button className="bg-pink-500 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-cyan-400 transition duration-150 ease-in-out">
+            <DarkMode toggleTheme={toggleTheme} />
+
+            </button>
+           
+         
           </div>
           {/* Responsive Menu */}
           <div className="md:hidden">
@@ -128,13 +143,12 @@ export default function Navbar() {
             </button>
             {/* Mobile Menu */}
             {isMenuOpen && (
-            <div className={`fixed top-16 left-0 right-0 bg-pink-800 p-4 text-white shadow-lg z-10`}>
-              <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Home</Link>
-            <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Testimonials</Link>
-            <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Price</Link>
-            <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Projects</Link>
-          </div>
-          
+              <div className={`fixed top-16 left-0 right-0 bg-pink-800 p-4 text-white shadow-lg z-10`}>
+                <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Home</Link>
+                <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Testimonials</Link>
+                <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Price</Link>
+                <Link href={"/"} className="block py-2 px-4 hover:bg-cyan-400">Projects</Link>
+              </div>
             )}
           </div>
         </div>
